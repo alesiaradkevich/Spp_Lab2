4c546b5949c451ccce19b64251ac9de28206f00b
alesiaradkevich <alesya.r.03@gmail.com>
2022-10-22 13:59:11 +0300

    implementation of plugins

---
+++ AdditionalGenerators/StrGenerator/FloatGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FakerLib;
+namespace StrGenerator
+{
+    public class FloatGenerator : IGenerator
+    {
+        public object Generate()
+        {
+            Random random = new Random();
+            float number = 0;
+            while (number == 0)
+            {
+                number = (float)random.NextDouble();
+            }
+            return number;
+        }
+
+        public Type GetGenerationType()
+        {
+            return typeof(float);
+        }
+    }
+}
